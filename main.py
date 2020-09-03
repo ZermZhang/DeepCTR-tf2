70e7405dd5ffc98b5d2d3d68c623153b87e3b459
zermzhang <zhang371312@126.com>
2020-09-03 08:26:17 +0800

    [feat]: 增加针对nlp的调用和测试过程
    1. 使用MNIST的数据对nlp进行测试，输出accuracy（0.97左右）
    2. 增加几个TODO List：
        1. 配置参数要抽象到对应的配置文件中
        2. 看看是否能够将调用过程抽象成一套统一的过程
        3. 针对tf.kears模型构建的另外一种方法也要实现

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import tensorflow as tf
 
-from models.linear import Linear
+from models import linear, mlp
+from datas import load_data
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+# TODO: try to abstract the call progress into a method
+def linear_runner():
+    """
+    the example runner for linear model
+    """
     X = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
     y = tf.constant([[10.0], [20.0]])
 
-    model = Linear()
+    model = linear.Linear()
     optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
 
     for i in range(100):
@@ -20,3 +24,47 @@ if __name__ == '__main__':
         optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
 
     print(model.variables)
+
+
+def mlp_runner():
+    """
+    the example runner for mlp model
+    """
+    # TODO: gather the config parameters into a configure file
+    num_epoches = 5
+    batch_size = 50
+    learning_rate = 0.001
+
+    model = mlp.MLP()
+    data_loader = load_data.MNISTLoader()
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+    num_batches = int(data_loader.num_train_data // batch_size * num_epoches)
+
+    # training
+    for batch_index in range(num_batches):
+        X, y = data_loader.get_batch(batch_size)
+        with tf.GradientTape() as tape:
+            y_pred = model(X)
+            loss = tf.keras.losses.sparse_categorical_crossentropy(y_true=y, y_pred=y_pred)
+            loss = tf.reduce_sum(loss)
+            print("batch {batch_index}: loss {loss}".format(batch_index=batch_index, loss=loss))
+
+        grads = tape.gradient(loss, model.variables)
+        optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
+
+    # evaluate
+    sparse_categorical_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+    num_batches = int(data_loader.num_test_data // batch_size)
+
+    for batch_index in range(num_batches):
+        start_index, end_index = batch_index * batch_size, (batch_index + 1) * batch_size
+        y_pred = model.predict(data_loader.test_data[start_index: end_index])
+        sparse_categorical_accuracy.update_state(y_true=data_loader.test_label[start_index: end_index], y_pred=y_pred)
+
+    print("test accuracy: {}".format(sparse_categorical_accuracy.result()))
+    return 0
+
+
+if __name__ == '__main__':
+    mlp_runner()
