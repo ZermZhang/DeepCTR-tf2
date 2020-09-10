b5e4693f57e55d8ce941fcab4d649839713dec6a
zermzhang <zhang371312@126.com>
2020-09-10 08:09:51 +0800

    [feat]: 增加nietzsche数据的读入和处理逻辑

--- datas/load_data.py
+++ datas/load_data.py
@@ -36,5 +36,25 @@ class MNISTLoader(object):
         return self.train_data[index, :], self.train_label[index]
 
 
+class RnnDataLoader(object):
+    def __init__(self):
+        path = tf.keras.utils.get_file('nietzsche.txt', origin='https://s3.amazonaws.com/text-datasets/nietzsche.txt')
+        with open(path, encoding='utf-8') as f:
+            self.raw_text = f.read().lower()
+        self.chars = sorted(list(set(self.raw_text)))
+        self.char_indices = dict((c, i) for i, c in enumerate(self.chars))
+        self.indices_char = dict((i, c) for i, c in enumerate(self.chars))
+        self.text = [self.char_indices[c] for c in self.raw_text]
+
+    def get_batch(self, seq_length, batch_size):
+        seq = []
+        next_char = []
+        for i in range(batch_size):
+            index = np.random.randint(0, len(self.text) - seq_length)
+            seq.append(self.text[index: index + seq_length])
+            next_char.append(self.text[index + seq_length])
+        return np.array(seq), np.array(next_char)
+
+
 if __name__ == "__main__":
     data_load = MNISTLoader()
