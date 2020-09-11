9e4148c059aeb99e36034d5a13fb132897dd2889
zermzhang <zhang371312@126.com>
2020-09-11 08:38:32 +0800

    [feat]: 初步实现wide&deep的代码
    1. 将linear的model和deep的model组合起来

--- models/wide_deep.py
+++ models/wide_deep.py
@@ -7,6 +7,22 @@
 @Author         : zermzhang
 @version        : 1.0
 @Desciption     : the wide and deep model
+    1. firstly, implementation the add losses between linear AND DNN
 """
 
+import tensorflow as tf
+
 from models import linear, mlp
+
+
+class WideAndDeep(tf.keras.Model):
+    def __init__(self, config):
+        super().__init__()
+        self.linear = linear.Linear()
+        self.dnn = mlp.DNN()
+
+    def call(self, input):
+        linear_output = self.linear(input)
+        dnn_output = self.dnn(input)
+        return linear_output, dnn_output
+
