53f5db6bd07aa532c7da955ed67383e81db2354e
zermzhang <zhang371312@126.com>
2020-09-20 21:33:02 +0800

    [test]: 增加rnn的相关评估代码

--- models/rnn.py
+++ models/rnn.py
@@ -9,6 +9,7 @@
 @Desciption     : the rnn model
 """
 import tensorflow as tf
+from datas import load_data
 
 
 class RNN(tf.keras.Model):
@@ -30,3 +31,30 @@ class RNN(tf.keras.Model):
             return logits
         else:
             return tf.nn.softmax(logits)
+
+
+def tester(CONFIG):
+    config_train = CONFIG.model_config
+
+    num_epoches = config_train.get('num_epoches', 3)
+    seq_length = config_train.get('seq_length', 40)
+    batch_size = config_train.get('batch_size', 100)
+    learning_rate = config_train.get('learning_rate', 0.01)
+    optimizer = config_train.get('optimizer', 'Adam')
+
+    data_loader = load_data.RnnDataLoader()
+    model = RNN(num_chars=len(data_loader.chars), batch_size=batch_size, seq_length=seq_length)
+    optimizer = tf.keras.optimizers.get(optimizer)(learning_rate=learning_rate)
+
+    for batch_index in range(num_epoches):
+        X, y = data_loader.get_batch(seq_length=seq_length, batch_size=batch_size)
+        with tf.GradientTape() as tape:
+            y_pred = model(X)
+            loss = tf.keras.losses.sparse_categorical_crossentropy(y_true=y, y_pred=y_pred)
+            loss = tf.reduce_sum(loss)
+            print("batch {}: loss {}".format(batch_index, loss.numpy()))
+
+        grads = tape.gradient(loss, model.variables)
+        optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
+
+    return 0
