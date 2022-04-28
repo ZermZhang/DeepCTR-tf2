947c6f3a4535b0d0299a9a0eba86d33a847512fe
ZermZhang <zhang371312@126.com>
2022-04-28 09:05:16 +0800

    [docs]: 新增代码注释

--- models/subclassing/__init__.py
+++ models/subclassing/__init__.py
@@ -24,60 +24,17 @@ class ModelBaseBuilder(tf.keras.Model):
         self.init_input_layer()
 
     def init_input_layer(self):
+        """
+        生成模型需要依赖的输入层信息
+        self.encoder_layers: preprocessing-layer，处理原始特征，转为编码格式
+        self.emb_layers: 对编码格式数据进行embedding处理，方便送入模型
+        self.all_inputs: 根据输入特征格式生成格式化输入说明，方便summary()和plot_model()
+        """
         for feature_name, feature_config in self.config.items():
             feature_builder = StaticEncodedFeatureBuilder(feature_name, feature_config)
             self.encoder_layers[feature_name] = feature_builder.feature_encoder
             self.emb_layers[feature_name] = feature_builder.emb_layer
             self.all_inputs[feature_name] = feature_builder.inputs
-
-    def build_graph(self):
-        return tf.keras.models.Model(
-            inputs=self.all_inputs,
-            outputs=self.call(self.all_inputs)
-        )
-
-
-
-
-
-class MLP(tf.keras.Model):
-    def __init__(self, config: dict, *args, **kwargs):
-        super(MLP, self).__init__(*args, **kwargs)
-        # init the feature config info
-        self.config = config
-        # definite the layers
-        self.dense_layer = tf.keras.layers.Dense(32, activation='relu')
-        self.dropout_layer = tf.keras.layers.Dropout(0.5)
-        self.output_layer = tf.keras.layers.Dense(1)
-
-        # init the preprocessing layer
-        self.encoders = {}
-        self.emb_layers = {}
-        self.all_inputs = {}
-
-    def build(self):
-        for feature_name, feature_config in self.config.items():
-            encoder_layer = StaticEncodedFeatureBuilder(feature_name, feature_config)
-            self.encoders[feature_name] = encoder_layer.feature_encoder
-            self.emb_layers[feature_name] = encoder_layer.emb_layer
-            self.all_inputs[feature_name] = encoder_layer.inputs
-
-    def call(self, inputs):
-        encoded_features = []
-        for feature_name, feature_config in self.config.items():
-            if not self.emb_layers[feature_name]:
-                encoded_features.append(
-                    self.encoders[feature_name](inputs[feature_name])
-                )
-            else:
-                encoded_features.append(
-                    self.emb_layers[feature_name](self.encoders[feature_name](inputs[feature_name]))
-                )
-        x = tf.keras.layers.concatenate(encoded_features)
-        x = self.dense_layer(x)
-        x = self.dropout_layer(x)
-        x = self.output_layer(x)
-        return x
 
     def build_graph(self):
         return tf.keras.models.Model(
