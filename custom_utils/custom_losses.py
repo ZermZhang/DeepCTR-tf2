5578d42a885886c28b3c8febb6ff638572a7fc84
ZermZhang <zhang371312@126.com>
2022-05-10 20:12:01 +0800

    [feat]: 新增loss函数

--- custom_utils/custom_losses.py
+++ custom_utils/custom_losses.py
@@ -27,3 +27,12 @@ class LossesFunc(object):
     @staticmethod
     def reduce_mean_squre_loss(y_true, y_pred):
         return tf.reduce_mean(tf.square(y_pred - y_true))
+
+    @staticmethod
+    def reduce_sum_sigmoid_crossentropy_loss(y_true, y_pred):
+        return tf.reduce_sum(
+            tf.nn.sigmoid_cross_entropy_with_logits(
+                labels=y_true,
+                logits=y_pred
+            )
+        )
