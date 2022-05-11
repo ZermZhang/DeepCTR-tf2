7e31c541bd18fa82a3ed6eb050f83dfb866a99bc
ZermZhang <zhang371312@126.com>
2022-05-11 09:18:43 +0800

    [feat]: 调整loss func获取逻辑

--- custom_utils/custom_losses.py
+++ custom_utils/custom_losses.py
@@ -9,17 +9,38 @@
 @Desciption     : 
 """
 import tensorflow as tf
+from tensorflow.python.util.tf_export import keras_export
+from tensorflow.keras.losses import deserialize
 
 
-class LossesFunc(object):
+@keras_export('keras.losses.get')
+def get(self, identifier):
+    if identifier is None:
+        return None
+
+    # some custom loss function
+    if identifier == 'reduce_sum_sparse_categorical_crossentropy':
+        return CustomLoss.reduce_sum_sigmoid_crossentropy_loss
+    if identifier == 'reduce_mean_squre_loss':
+        return CustomLoss.reduce_mean_squre_loss
+    if identifier == 'reduce_sum_sigmoid_crossentropy_loss':
+        return CustomLoss.reduce_sum_sigmoid_crossentropy_loss
+
+    # the builtin loss function
+    if isinstance(identifier, str):
+        identifier = str(identifier)
+        return deserialize(identifier)
+    if isinstance(identifier, dict):
+        return deserialize(identifier)
+    if callable(identifier):
+        return identifier
+    raise ValueError(f'Could not interpret loss function identifier: {identifier}')
+
+
+class CustomLoss:
     """
-    the custom loss function
+    some custom loss function
     """
-    def __init__(self, name="reduce_sum_sparse_categorical_crossentropy"):
-        self.name = name
-        if self.name == "reduce_sum_sparse_categorical_crossentropy":
-            self.loss = self.reduce_sum_sparse_categorical_crossentropy
-
     @staticmethod
     def reduce_sum_sparse_categorical_crossentropy(y_true, y_pred):
         return tf.reduce_sum(tf.keras.losses.sparse_categorical_crossentropy(y_true=y_true, y_pred=y_pred))
@@ -30,9 +51,4 @@ class LossesFunc(object):
 
     @staticmethod
     def reduce_sum_sigmoid_crossentropy_loss(y_true, y_pred):
-        return tf.reduce_sum(
-            tf.nn.sigmoid_cross_entropy_with_logits(
-                labels=y_true,
-                logits=y_pred
-            )
-        )
+        return tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(labels=y_true, logits=y_pred))
