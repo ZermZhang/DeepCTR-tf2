de3d7d5042fca60fe93ea00a33627cba722ce86f
ZermZhang <zhang371312@126.com>
2022-07-22 09:02:45 +0800

    [feat]: 梳理配置文件的相关信息，统一配置信息出口

--- src/utils/config.py
+++ src/utils/config.py
@@ -38,6 +38,57 @@ class Config(object):
             config = yaml.safe_load(f)
         return config
 
+    # =============================================================
+    # 配置信息的出口
+    # =============================================================
+    @property
+    def dataset_config(self):
+        """
+        数据集的相关配置，主要包括：
+        1. 数据集的基本配置信息（schema，default_value德国）
+        2. 模型训练中需要用到的feature_col信息
+        """
+        base_dataset_config = self.config.get('dataset', {})
+        all_column_names, all_column_defaults, valid_feature_columns, sequence_columns = self.get_column_default()
+        label_name = [key for key, value in self.feature_config.items() if value['type'] == 'label'][0]
+
+        base_dataset_config['all_column_names'] = all_column_names
+        base_dataset_config['all_column_defaults'] = all_column_defaults
+        base_dataset_config['valid_feature_columns'] = valid_feature_columns
+        base_dataset_config['label_name'] = label_name
+        base_dataset_config['sequence_columns'] = sequence_columns
+
+        return base_dataset_config
+
+    @property
+    def model_config(self):
+        """
+        模型的基本配置，网络结构等信息
+        """
+        return self.config.get('model', {})
+
+    @property
+    def train_config(self):
+        """
+        训练过程需要的配置，optimizer，loss，learning_rate等
+        """
+        return self.config.get('train', {})
+
+    @property
+    def preprocessing_config(self):
+        """
+        preprocessing_layer的相关配置，包括preprocessing_layer和embedding_layer
+        """
+        features_config_ = self.feature_config
+        preprocessing_config_ = {}
+        for feature_name, config in features_config_.items():
+            if config['type'] not in ('label', 'no-use'):
+                preprocessing_config_.update({feature_name: config})
+        return preprocessing_config_
+
+    # =============================================================
+    # =============================================================
+
     def get_column_default(self):
         column_names = []
         column_defaults = []
@@ -69,47 +120,6 @@ class Config(object):
                 sequence_columns.append(name)
 
         return column_names, column_defaults, valid_columns, sequence_columns
-
-    # the dataset config info from configure file
-    @property
-    def dataset_config(self):
-        base_dataset_config = self.config.get('dataset', {})
-        all_column_names, all_column_defaults, valid_feature_columns, sequence_columns = self.get_column_default()
-        label_name = [key for key, value in self.feature_config.items() if value['type'] == 'label'][0]
-
-        base_dataset_config['all_column_names'] = all_column_names
-        base_dataset_config['all_column_defaults'] = all_column_defaults
-        base_dataset_config['valid_feature_columns'] = valid_feature_columns
-        base_dataset_config['label_name'] = label_name
-        base_dataset_config['sequence_columns'] = sequence_columns
-
-        return base_dataset_config
-
-    # the model config info from configure file
-    @property
-    def model_config(self):
-        return self.config.get('model', {})
-
-    @property
-    def train_config(self):
-        return self.config.get('train', {})
-
-    @property
-    def deep_model_config(self):
-        return self.model_config.get('deep', {})
-
-    @property
-    def wide_model_config(self):
-        return self.model_config.get('wide', {})
-
-    @property
-    def preprocessing_config(self):
-        features_config_ = self.feature_config
-        preprocessing_config = {}
-        for feature_name, config in features_config_.items():
-            if config['type'] not in ('label', 'no-use'):
-                preprocessing_config.update({feature_name: config})
-        return preprocessing_config
 
     # the detail info for data config
     def read_data_path(self, mode='train'):
